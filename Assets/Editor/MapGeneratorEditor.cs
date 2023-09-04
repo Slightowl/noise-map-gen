f55f830a5631af022c99480c2c94e52e4c327056
Slightowl <slightowler7@gmail.com>
2023-09-04 04:59:43 +0100

    commit

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI() 
+    {
+        MapGenerator mapGenerator = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGenerator.autoUpdate) 
+            {
+                mapGenerator.GenerateMap();
+            }
+        }
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button ("Generate")) {
+            mapGenerator.GenerateMap();
+        }
+    }
+}
